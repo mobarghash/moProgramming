ee25d27f0cc0da844cadf5951303a8186e2340ac
mobarghash <mo.barghash@gmail.com>
2015-12-26 21:48:04 +1100

    This is my submission after editing the lecturer file

---
+++ cachematrix.R
@@ -0,0 +1,13 @@
+## The purposse of thes two function is to calculate the invert of a matrix, 
+##then save the out come in a cach. The second function will test if the 
+## the catch has the value of the invert, if not it will calculate it again.
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+}
+
+## This function creates a special "matrix" object that can cache its inverse
+
+cacheSolve <- function(x, ...) {
+  ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+}
